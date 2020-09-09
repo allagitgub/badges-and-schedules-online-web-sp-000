0a5dfb6a8e1bdf16f6ad2be121b34607face793f
Alla Kuzminova <alchik@gmail.com>
2020-09-09 00:20:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,10 @@ def batch_badge_creator(attendees)
   attendees.collect { |name| badge_maker(name) }
 end
 
-def assign_rooms
+def assign_rooms(attendees)
+  attendees.each_with_index { |name, index|  "Hello, #{name}! You'll be assigned to room #{index}!}
 end
 
-def printer
+def printer(ettendees)
+batch_badge_creator(attendees).each { |badge| puts badge}
 end
