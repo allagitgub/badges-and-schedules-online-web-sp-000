f71de837aa9162429af586770e8a99285b37b0ee
Alla Kuzminova <alchik@gmail.com>
2020-09-09 00:31:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,8 @@ end
 def assign_rooms(attendees)
   room_assignments = Array.new
   attendees.each_with_index { |name, index|
-    room = index + 1
-    room_assignments[room] = "Hello, #{name}! You'll be assigned to room #{index}!"
+    room =
+    room_assignments[index] = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   }
   room_assignments
 end
