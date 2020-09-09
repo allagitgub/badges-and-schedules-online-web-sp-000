e2f16b5bae3382460d0453e5f6af70e80e60122b
Alla Kuzminova <alchik@gmail.com>
2020-09-09 00:11:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,9 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
-def batch_badge_creator(array)
 
+def batch_badge_creator(attendees)
+  attendees.collect { |name| badge_maker(name) }
 end
 
 def assign_rooms
